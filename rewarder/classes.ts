598064d67bd1b486d5b8ad04b097abd7490f48e5
Dana <dana@step.finance>
2021-10-20 12:05:51 -0400

    BN fix

--- rewarder/classes.ts
+++ rewarder/classes.ts
@@ -7,16 +7,16 @@ export class PayerAmount {
     nonStepMint: string;
     token: string;
     payer: string;
-    amount: any; //BN
-    stepMultiplier: any; //BN
-    stepAmount: any; //BN
-    constructor(pool: string, nonStepMint: string, token: string, payer: string, amount: any, stepMultiplier: any) {
+    amount: typeof BN; 
+    stepMultiplier: typeof BN; 
+    stepAmount: typeof BN; 
+    constructor(pool: string, nonStepMint: string, token: string, payer: string, amount: typeof BN, stepMultiplier: typeof BN) {
         this.pool = pool;
         this.nonStepMint = nonStepMint;
         this.token = token;
         this.payer = payer;
-        this.amount = (amount instanceof BN) ? amount : new BN(amount); //BN
-        this.stepMultiplier = (stepMultiplier instanceof BN) ? stepMultiplier : new BN(stepMultiplier); //BN
+        this.amount = (amount instanceof BN) ? amount : new BN(amount); 
+        this.stepMultiplier = (stepMultiplier instanceof BN) ? stepMultiplier : new BN(stepMultiplier);
         this.stepAmount = this.amount.mul(this.stepMultiplier).div(U64_MAX);
     }
 }
@@ -34,8 +34,8 @@ export class PoolFeesPaid {
         this.payers = [];
     }
 
-    addPayer(pubkey: string, amount: any /* BN */) {
-        amount = (amount instanceof BN) ? amount : new BN(amount); //BN
+    addPayer(pubkey: string, amount: typeof BN) {
+        amount = (amount instanceof BN) ? amount : new BN(amount); 
         this.payers.push(
             new PoolFeePayer(
                 pubkey, 
